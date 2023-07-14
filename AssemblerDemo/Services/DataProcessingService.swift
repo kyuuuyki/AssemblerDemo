73d1a5ad11043cee2079cee2654dceb193cee469
Chayanon Ardkham <develop@chayanon.dev>
2023-07-14 14:32:23 +0700

    Documentation

--- AssemblerDemo/Services/DataProcessingService.swift
+++ AssemblerDemo/Services/DataProcessingService.swift
@@ -7,6 +7,25 @@ import Foundation
 import KyuGenericExtensions
 
 public protocol DataProcessingServiceProtocol: ModuleProtocol {
+	/// Generate a unique string prefixed and suffixed by specified strings.
+	///
+	/// Generate a unique string using [UUID().uuidString](https://developer.apple.com/documentation/foundation/uuid/1779712-uuidstring) then prefixs and suffixs it by specified strings.
+	///
+	/// ```swift
+	/// let dataProcessingService = DataProcessingService()
+	/// let uniqueString = try? dataProcessingService.generateUniqueString(
+	/// 	prefix: "Prefix",
+	/// 	suffix: "Suffix"
+	/// )
+	/// ```
+	///
+	/// - Parameters:
+	/// 	- prefix: User-specified prefix string.
+	/// 	- suffix: User-specified suffix string.
+	///
+	/// - Returns: A uniquely generated string encased with specified prefix and suffix strings.
+	///
+	/// - Throws: Throws an error if the specified prefix or suffix is empty.
 	func generateUniqueString(prefix: String, suffix: String) throws -> String
 }
 
